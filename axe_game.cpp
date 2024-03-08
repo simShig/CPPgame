e64804f45f18cd12dc5dbb3ebf189d885dfcdedd
simShig <simshigol@gmail.com>
2024-03-08 12:15:15 +0200

    finished part 16

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-    InitWindow(350,200,"MyTitle");
+    float root_beer = 1.99;
+    double CheeseBurger{5.99};
+    bool shouldHaveLunch = true;
+    int width;
+    width = 350;
+    InitWindow(width,200,"MyTitle");
+    while(true){}
 }
