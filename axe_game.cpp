2e5341ad707cf12858cacf801ad4b3790abd448e
simShig <simshigol@gmail.com>
2024-03-10 20:50:10 +0200

    Creating circle and ectangle (Axe-game)

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,11 +2,40 @@
 
 int main()
 {
-    float root_beer = 1.99;
-    double CheeseBurger{5.99};
-    bool shouldHaveLunch = true;
-    int width;
-    width = 350;
-    InitWindow(width,200,"MyTitle");
-    while(true){}
+  // window Properties:
+
+
+    int width{800};
+    int height{450};
+    InitWindow(width, height, "MyWindow");
+
+    // circle coordinates
+
+    int circle_x{200};
+    int circle_y{200};
+
+    // axe coordinates
+    int axe_x{400};
+    int axe_y{0};
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false)
+        // Game logic starts:
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+
+        // move the axe
+        axe_y += 10;
+
+        if (IsKeyDown(KEY_D) && circle_x < 350)
+        {
+            circle_x += 10;
+        }
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x -= 10;
+        }
+
+        // Game logic ends
 }
