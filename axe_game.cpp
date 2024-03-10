3f33c0b0d7f559cfca256053e4f18d10d8772359
simShig <simshigol@gmail.com>
2024-03-10 22:29:50 +0200

    FINISHED AXE GAME!!!

--- axe_game.cpp
+++ axe_game.cpp
@@ -30,39 +30,60 @@ int main()
     int b_axe_y{axe_y+axe_length};     //lower edge
 
     int direction{10};
+    bool collision_with_axe{false};
 
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false)
-        // Game logic starts:
-    {
-        BeginDrawing();
-        ClearBackground(WHITE);
-
-
-        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
-
-        // move the axe
-        axe_y += direction;
-
-        if(axe_y>height || axe_y<0){
-            direction = -direction;
+    {   
+        collision_with_axe = b_axe_y>=u_circle_y && u_axe_y<=b_circle_y && l_axe_x<=r_circle_x && r_axe_x>=l_circle_x;
+        //update edge location:
+        l_circle_x=circle_x-circle_radius;
+        r_circle_x=circle_x+circle_radius;
+        u_circle_y=circle_y-circle_radius;
+        b_circle_y=circle_y+circle_radius;
+        l_axe_x = axe_x;
+        r_axe_x = axe_x+axe_length;
+        u_axe_y = axe_y;
+        b_axe_y = axe_y+axe_length;
+        //check collision:        
+        if (collision_with_axe){
+            ClearBackground(BLACK);
+            DrawText("GAME OVER!!!",400,200,20,RED);
         }
+        else{
 
-        //check for collision:
+
+            // Game logic starts:
+            BeginDrawing();
+            ClearBackground(WHITE);
 
 
-        if (IsKeyDown(KEY_D) && circle_x < width)
-        {
-            circle_x += 10;
-        }
-        if (IsKeyDown(KEY_A) && circle_x > 0)
-        {
-            circle_x -= 10;
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
+            // move the axe
+            axe_y += direction;
+
+            if(axe_y>height || axe_y<0){
+                direction = -direction;
+            }
+
+            //check for collision:
+
+
+            if (IsKeyDown(KEY_D) && circle_x < width)
+            {
+                circle_x += 10;
+            }
+            if (IsKeyDown(KEY_A) && circle_x > 0)
+            {
+                circle_x -= 10;
+            }
         }
         // Game logic ends
         EndDrawing();
     }
+    
 
 }
